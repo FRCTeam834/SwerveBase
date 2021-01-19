3c70dee68365396fd057b5e3d3fde30f11c47846
CAP1Sup <42127153+CAP1Sup@users.noreply.github.com>
2021-01-19 17:09:48 -0500

    Added documentation to driver parameters

--- src/main/java/frc/robot/DriverProfiles/DriverProfile.java
+++ src/main/java/frc/robot/DriverProfiles/DriverProfile.java
@@ -12,6 +12,15 @@ import com.revrobotics.CANSparkMax.IdleMode;
 
 /**
  * A quick way of referencing driver parameters
+ * 
+ * @param NAME                The name of the driver
+ * @param JOYSTICK_DEADZONE   The deadzone of the joysticks. Joystick values range from -1 to 1, so a 0.1 deadzone is usually sufficent to deal with any wobble
+ * @param TURN_SCALE          The multiplier on how quickly to turn. Needs to be tested
+ * @param DRIVE_RAMP_RATE     The speed at which the drivetrain ramps to full speed. Prevents sudden jerks. Maybe something to override?
+ * @param LOCKEM_UP           If the swerve should lock the modules at 45 degrees, effectivly hitting the brakes. Hard on the modules, but worth it in comp
+ * @param FIELD_CENTRIC       If the swerve should treat itself as forward or if the field's forward should be forward
+ * @param MAX_SPEED           Software limiting of speeds for testing purposes
+ * @param DRIVE_IDLE_MODE     If the drive motors should coast or brake after they are spinning too fast. Coasting makes the driving smoother, but braking makes it more aggressive
  */
 public class DriverProfile {
     public String NAME;
